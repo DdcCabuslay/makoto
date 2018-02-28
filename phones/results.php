1be82728cabfa7b5184e21df17bf8e3abb832065
Daniel Cabuslay <ddccabuslay@gmail.com>
2018-02-28 03:54:49 +0000

    Added DB connection

--- phones/results.php
+++ phones/results.php
@@ -1,34 +1,31 @@
+<?php
+	$connectString = "dbname=ddccabuslay user=" . $_SERVER['DB_USER'];
+	$connection = pg_connect($connectString) or die('Could not connect: ' . pg_last_error());
+	$dbQuery = "select phone.id, oem.name, model, min(price) as price from makoto.oem, makoto.phone, makoto.phone_pricing, makoto.vendor where oem_id = oem.id and phone.id = phone_pricing.phone_id and vendor.is_carrier = FALSE and vendor.id = vendor_id group by phone.id, oem.name";
+	$results = pg_query($connection, $dbQuery);
+	$results_array = array();
+	$counter = 0;
+	while ($row = pg_fetch_array($results)) {
+		$results_array[$counter]['id'] = $row['id'];
+		$results_array[$counter]['model'] = $row['model'];
+		$results_array[$counter]['oem'] = $row['name'];
+		$results_array[$counter]['price'] = $row['price'];
+		$counter++;
+	}
+?>
 <div class="item_grid">
-	<a href="phone.php" class="item">
+	<?php foreach ($results_array as $result): ?>
+	<a href="phone.php?id=<?=$result['id']?>" class="item">
 		<img class="item_img" src="../images/phones/0/0.png">
 		<div class="item_info">
-			<div class="item_name">Pixel 2 XL</div>
-			<span class="item_oem">Google</span>
-			<span class="item_primary_price">$899</span>
+			<div class="item_name">
+				<?=$result['model']?>
+			</div>
+			<span class="item_oem">
+				<?=$result['oem']?>
+			</span>
+			<span class="item_primary_price">$<?=$result['price']?></span>
 		</div>
 	</a>
-	<div class="item">
-		<img class="item_img" src="../images/phones/1/0.png">
-		<div class="item_info">
-			<a class="item_name">5T</a>
-			<span class="item_oem">OnePlus</span>
-			<span class="item_primary_price">$659</span>
-		</div>
-	</div>
-	<div class="item">
-		<img class="item_img" src="../images/phones/2/0.png">
-		<div class="item_info">
-			<a class="item_name">iPhone X</a>
-			<span class="item_oem">Apple</span>
-			<span class="item_primary_price">$1319</span>
-		</div>
-	</div>
-	<div class="item">
-		<img class="item_img" src="../images/phones/3/0.png">
-		<div class="item_info">
-			<a class="item_name">Galaxy S8</a>
-			<span class="item_oem">Samsung</span>
-			<span class="item_primary_price">$1035</span>
-		</div>
-	</div>
+	<?php endforeach;?>
 </div>
