0aa2d72028b56a5ba68b3f969ed3274f427a4e0c
Daniel Cabuslay <ddccabuslay@gmail.com>
2018-02-28 14:24:08 -0500

    Moved header to separate file

---
+++ phones/header.php
@@ -0,0 +1,23 @@
+<header class="site_header">
+	<section>
+		<a href="../" class="header_logo">Makoto</a>
+		<nav class="site_nav">
+			<a class="nav_selected" href=".">
+				<i class="fas fa-mobile fa-fw"></i>
+				<span class="nav_title">Phones</span>
+			</a>
+			<a href="../laptops">
+				<i class="fas fa-laptop fa-fw"></i>
+				<span class="nav_title">Laptops</span>
+			</a>
+		</nav>
+	</section>
+	<section>
+		<form class="search_form">
+			<input placeholder="Search Makoto">
+			<button class="search_button">
+				<i class="fas fa-search fa-fw"></i>
+			</button>
+		</form>
+	</section>
+</header>
