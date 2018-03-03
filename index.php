256ba56a01046b9a1221d989ff4301e22336c670
Daniel Cabuslay <ddccabuslay@gmail.com>
2018-03-03 15:39:27 -0500

    Changed featured phones on home page

--- index.php
+++ index.php
@@ -15,39 +15,49 @@
 	<main>
 		<div class="hero home_hero_main">
 			<h1>Choosing a phone can be hard.</h1>
-			<h2>Hopefully I can help.</h2>
+			<h2>Let's help you find one.</h2>
 			<div class="home_hero_button_row">
 				<a class="button filled_button" href="phones">Browse Phones</a>
 			</div>
 		</div>
 		<div class="hero home_hero_grid">
-			<div class="home_hero" style="background-color: #000;">
-				<div class="home_hero_info">
-					<h2 style="color: #fff;">Samsung Galaxy S9</h2>
-					<a href="phones/phone.php?id=10" class="button stroked_button" style="color: #fff; border-color: #fff;">More Info</a>
-				</div>
-				<img src="images/home/s9.jpg">
-			</div>
 			<div class="home_hero" style="background-color: #f4f4f4;">
 				<div class="home_hero_info">
-					<h2>Google Pixel 2 XL</h2>
-					<a href="phones/phone.php?id=0" class="button stroked_button">More Info</a>
+					<h2>Google Pixel 2</h2>
+					<div>
+						<a href="phones/phone.php?id=1" class="button stroked_button">Pixel 2</a>
+						<a href="phones/phone.php?id=0" class="button stroked_button">Pixel 2 XL</a>
+					</div>
 				</div>
-				<img src="images/home/0.jpg">
+				<img src="images/home/pixel2.jpg">
+			</div>
+			<div class="home_hero" style="background-color: #fceee5;">
+				<div class="home_hero_info">
+					<h2>Apple iPhone 8</h2>
+					<div>
+						<a href="phones/phone.php?id=5" class="button stroked_button">iPhone 8</a>
+						<a href="phones/phone.php?id=6" class="button stroked_button">iPhone 8 Plus</a>
+					</div>
+				</div>
+				<img src="images/home/iphone8.jpg">
+			</div>
+			<div class="home_hero" style="background-color: #fff;">
+				<div class="home_hero_info">
+					<h2>LG V30</h2>
+					<div>
+						<a href="phones/phone.php?id=10" class="button stroked_button">More Info</a>
+					</div>
+				</div>
+				<img src="images/home/lgv30.jpg">
 			</div>
 			<div class="home_hero" style="background-color: #2b2b2b;">
 				<div class="home_hero_info">
 					<h2 style="color: #d8d8d8;">Essential Phone</h2>
-					<a href="phones/phone.php?id=9" class="button stroked_button" style="color: #d8d8d8; border-color: #d8d8d8;">More Info</a>
+					<div>
+						<a href="phones/phone.php?id=9" class="button stroked_button" style="color: #d8d8d8; border-color: #d8d8d8;">More Info</a>
+					</div>
 				</div>
-				<img src="images/home/9.jpg">
-			</div>
-			<div class="home_hero" style="background-color: #fafafa;">
-				<div class="home_hero_info">
-					<h2>Apple iPhone X</h2>
-					<a href="phones/phone.php?id=2" class="button stroked_button">More Info</a>
-				</div>
-				<img src="images/home/2.jpg">
+				<img src="images/home/essential.jpg">
 			</div>
 		</div>
 	</main>
